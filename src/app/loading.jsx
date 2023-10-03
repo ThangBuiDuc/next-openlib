84113e7ec36d2e9e70f25ade045235f8b29f0b61
ThangBuiDuc <thangbd@hpu.edu.vn>
2023-10-03 14:41:04 +0700

    add loading

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <span className="loading loading-infinity w-[100px]"></span>
+    </div>
+  );
+}
