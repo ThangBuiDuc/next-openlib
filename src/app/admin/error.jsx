e8434bd94ed5a7db8f9c0aef74ff22ab848b446c
ThangBuiDuc <thangbd@hpu.edu.vn>
2023-12-16 09:30:34 +0700

    merge

--- src/app/admin/error.jsx
+++ src/app/admin/error.jsx
@@ -1,6 +1,6 @@
 "use client"; // Error components must be Client Components
 
-export default function Error() {
+export default function Error(err) {
   return (
     <div>
       <h2>Something went wrong!</h2>
