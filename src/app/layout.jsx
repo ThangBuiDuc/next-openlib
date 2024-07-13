21f9a62b6550330bd72126adc384e42d218f150a
ThangBuiDuc <thangbd@hpu.edu.vn>
2024-07-13 09:51:28 +0700

    add GA

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,7 @@ import ReactQueryProvider from "./reactQueryProvider";
 import localFont from "next/font/local";
 import { viVN } from "@clerk/localizations";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const montserrat = localFont({
   src: [
@@ -26,6 +27,16 @@ export default function RootLayout({ children }) {
       <ClerkProvider localization={viVN}>
         <html lang="vi">
           <body className={`${montserrat.className} bg-white `}>
+            <Script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-Z7Y9M9XWNW"
+            ></Script>
+            <Script>
+              window.dataLayer = window.dataLayer || []; function gtag()
+              {dataLayer.push(arguments)}
+              gtag('js', new Date()); gtag('config', 'G-Z7Y9M9XWNW');
+            </Script>
+
             {children}
             <SpeedInsights />
           </body>
